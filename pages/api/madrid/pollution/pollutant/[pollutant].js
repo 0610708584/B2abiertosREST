3888f8b906fa3aefe108d1f0aace1eb183aedb61
0610708584 <80173386+0610708584@users.noreply.github.com>
2022-10-29 20:14:42 +0200

    ignore case de contaminante

--- pages/api/madrid/pollution/pollutant/[pollutant].js
+++ pages/api/madrid/pollution/pollutant/[pollutant].js
@@ -11,7 +11,7 @@ export default async(req, res) => {
 							           .then(response => response.text())
 				var pollutions = JSON.parse(data)
 
-        var pollution = pollutions.filter(function(pollutions) { return pollutions.Contaminante == pollutant});
+        var pollution = pollutions.filter(function(pollutions) { return pollutions.Contaminante.toLowerCase() == pollutant.toLowerCase()});
 				return res.status(200).json(pollution)
 			} catch(error) {
 				return res.status(400).json({error: error.message})
