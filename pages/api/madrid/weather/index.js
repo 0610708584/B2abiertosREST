93ede7ad3d4eb519c67e87db463a524d5b08c49c
0610708584 <80173386+0610708584@users.noreply.github.com>
2022-12-09 14:32:42 +0100

    commit

--- pages/api/madrid/weather/index.js
+++ pages/api/madrid/weather/index.js
@@ -7,7 +7,8 @@ export default async function handler(req, res) {
 				var data = await fetch('https://datos.alcobendas.org/dataset/083e08c7-1647-4bb1-a64b-957d3604f346/resource/9f58d5b2-9d2d-4a1c-91eb-c9c34b2b7614/download/recurso.json')
 							           .then(response => response.text())
 				var weather = JSON.parse(data.substring(1))
-				return res.status(200).json(weather.records)
+				var records = weather.records
+				return res.status(200).json(records)
 			} catch(error) {
 				return res.status(400).json({error: error.message})
 			}
